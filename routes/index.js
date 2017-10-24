e627df376b81bbea8b1879f2aad5f7a77008d4ec
Gareth Gomersall <gareth.gomersall@medicanimal.com>
2017-10-24 14:03:39 +0100

    added basic admin page

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-res.render('hello', { 
-    name: 'wes',
-    dog: 'dix'
+// res.render('hello', { 
+//     name: 'wes',
+//     dog: 'dix'
+// })
+    res.render('hello')
 })
+router.get('/admin', (req, res) => {
+    res.render('admin')
 })
 
 module.exports = router;
