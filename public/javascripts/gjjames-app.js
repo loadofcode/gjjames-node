23041dcc42fa334c8676910f4aaa41a9252c36de
Gareth <ggomersall@gmail.com>
2018-02-15 22:40:37 +0000

    added font-awesome to project - and using edit icon as button

--- public/javascripts/gjjames-app.js
+++ public/javascripts/gjjames-app.js
@@ -3,5 +3,6 @@ import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 // import autocomplete from './modules/autocompete';
 import typeAhead from './modules/typeAhead';
+import './external/fontawesome-all';
 
 typeAhead( $('.search') );
