238695b0dcf6dc938eff5d2b84a3c9eaddfdcd00
Gareth <ggomersall@gmail.com>
2018-05-18 17:24:50 +0100

    added product enquire form - need to hook up mailjet to it

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -3000,14 +3000,54 @@ var _typeAheadHome = __webpack_require__(13);
 
 var _typeAheadHome2 = _interopRequireDefault(_typeAheadHome);
 
+__webpack_require__(40);
+
 __webpack_require__(10);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _typeAhead2.default)((0, _bling.$)('.search'));
 // import autocomplete from './modules/autocompete';
-
+(0, _typeAhead2.default)((0, _bling.$)('.search'));
 (0, _typeAheadHome2.default)((0, _bling.$)('.search__hero'));
+
+/***/ }),
+/* 34 */,
+/* 35 */,
+/* 36 */,
+/* 37 */,
+/* 38 */,
+/* 39 */,
+/* 40 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
 
 /***/ })
 /******/ ]);
--- public/javascripts/gjjames-app.js
+++ public/javascripts/gjjames-app.js
@@ -4,6 +4,7 @@ import { $, $$ } from './modules/bling';
 // import autocomplete from './modules/autocompete';
 import typeAhead from './modules/typeAhead';
 import typeAheadHome from './modules/typeAheadHome';
+import './modules/productModal';
 import './external/fontawesome-all';
 
 typeAhead( $('.search') );
