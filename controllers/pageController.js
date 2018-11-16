b23d843ca7855da7a1b28a2961914fa1ca68f702
Gareth Gomersall <ggomersall@gmail.com>
2018-11-16 13:23:03 +0000

    updated the contact forms and homepage forms to include captcha

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,4 +1,7 @@
 const mail = require('../handlers/mail');
+const request = require('request');
+
+
 
 exports.homePage = (req, res) => {
   res.render('index', { title: 'Home' });
@@ -26,20 +29,45 @@ exports.websiteEnquiry = async (req, res) => {
   const telephone = req.body.phone.trim()
   const company = req.body.company.trim()
   const message = req.body.message
-  await mail.send({
-      from: 'info@gjjames.co.uk',
-      replyTo: email,
-      to: 'gareth@gjjames.co.uk',
-      subject: 'Website enquiry',
-      email,
-      name,
-      telephone,
-      company,
-      message,
-      filename: 'website-enquire'
-    })
-  req.flash('success', `Thank you for contacting us, we'll be in touch soon`);
-  res.redirect('/');
+  const captcha = req.body['g-recaptcha-response'];
+  const secret = process.env.CAPTCHA_SECRET;
+  const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${captcha}&remoteip=${req.connection.remoteAddress}`;
+
+
+  if (captcha === undefined || captcha === '' || captcha === null) {
+    await req.flash('error', `Please tick the captcha box`);
+    return res.redirect('/')
+  }
+
+  await request(verificationURL, async function(error, response, body) {
+    body = JSON.parse(body);
+
+    if(body.success !== undefined && !body.success) {
+      await req.flash('error', `Please tick the captcha box`);
+      return res.redirect('/')
+    }
+
+    if (body.success) {
+      await mail.send({
+        from: 'info@gjjames.co.uk',
+        replyTo: email,
+        to: 'gareth@gjjames.co.uk',
+        subject: 'Website enquiry',
+        email,
+        name,
+        telephone,
+        company,
+        message,
+        filename: 'website-enquire'
+      })
+      req.flash('success', `Thank you for contacting us, we'll be in touch soon`);
+      res.redirect('/');
+    }
+    
+  })
+
+
+  
 }
 
 exports.contactPageForm = async (req, res) => {
@@ -48,17 +76,40 @@ exports.contactPageForm = async (req, res) => {
   const subject = req.body.subject.trim()
   const company = req.body.company.trim()
   const message = req.body.message
-  await mail.send({
-      from: 'info@gjjames.co.uk',
-      replyTo: email,
-      to: 'gareth@gjjames.co.uk',
-      subject: subject,
-      email,
-      name,
-      company,
-      message,
-      filename: 'contact-page-form'
-    })
-  req.flash('success', `Thank you for contacting us, we'll be in touch soon`);
-  res.redirect('/stock1234/contact');
+
+  const captcha = req.body['g-recaptcha-response'];
+  const secret = process.env.CAPTCHA_SECRET;
+  const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${captcha}&remoteip=${req.connection.remoteAddress}`;
+
+  if (captcha === undefined || captcha === '' || captcha === null) {
+    await req.flash('error', `Please tick the captcha box`);
+    return res.redirect('/stock1234/contact')
+  }
+
+  await request(verificationURL, async function(error, response, body) {
+    body = JSON.parse(body);
+
+    if(body.success !== undefined && !body.success) {
+      await req.flash('error', `Please tick the captcha box`);
+      return res.redirect('/stock1234/contact')
+    }
+
+    if (body.success) {
+      await mail.send({
+        from: 'info@gjjames.co.uk',
+        replyTo: email,
+        to: 'gareth@gjjames.co.uk',
+        subject: subject,
+        email,
+        name,
+        company,
+        message,
+        filename: 'contact-page-form'
+      })
+    req.flash('success', `Thank you for contacting us, we'll be in touch soon`);
+    res.redirect('/stock1234/contact');
+    }
+    
+  })
+  
 }
